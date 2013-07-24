cb1d6269179ccc838dfc6042623320dd307af18d
john <john@john-desktop.(none)>
2013-07-24 10:18:07 +0000

    Adding documentation

---
+++ documentation/CodingConvention_FunctionTemplate.m
@@ -0,0 +1,50 @@
+% <a one-sentence description>
+%
+% <Octave compatible>
+%
+% Description
+%  <a full description of the function>
+%
+% Inputs
+%  <variable name> : [unit] [dimension] <description>
+%  <variable name> : [unit] [dimension] <description>
+% (<variable name>): [unit] [dimension] <description of an optional variable>
+%                    (def. <default value>)
+%
+% Outputs
+%  <variable name> : [unit] [dimension] <description>
+%  <variable name> : [unit] [dimension] <description>
+%
+% Example
+%  <a few minimal matlab lines to show the simple usage of the function>
+%
+% References
+%  [1] A. Roebel, and X. Rodet, "Efficient Spectral Envelope Estimation And Its
+%      Application To Pitch Shifting And Envelope Preservation", Proc. Digital
+%      Audio Effects (DAFx), DAFX, 30-35, 2005
+%  [2] S. Imai, "Cepstral analysis synthesis on the mel frequency scale", Proc.
+%      IEEE International Conference on Acoustics, Speech, and Signal Processing
+%      (ICASSP), vol 8, pp 93-96, 1983.
+%
+% Copyright (c) <year> <your institution>
+%
+% License
+%  <A short mention of the license of this function.
+%  This is absolutely necessary since the licenses may vary between files.>
+%
+% This function is part of the Common Speech Processing Repository (TODO)
+% TODO URL
+% 
+% Author <only the initial contributors of the code>
+%  Firstname Lastname <email@yourinstitution.com>
+%
+% $Id <info set by the versioning system> $
+
+function ...
+
+return
+
+% Changes
+% <date with format YYYY-MM-DD> <change's author <email@yourinstitution.com> >
+%  <exhaustive comment describing any modification made to the file>
+%
