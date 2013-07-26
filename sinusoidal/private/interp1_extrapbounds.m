5e5a935889d1404dfc352ba3a423178e36d93ed6
Gilles Degottex <degottex@csd.uoc.gr>
2013-07-26 21:35:04 +0300

    Fix header

--- sinusoidal/private/interp1_extrapbounds.m
+++ sinusoidal/private/interp1_extrapbounds.m
@@ -1,3 +1,23 @@
+% Replace NaN values at boundaries of a vector by constant extrapolation
+% 
+% Copyright (c) 2012 University of Crete - Computer Science Department
+% 
+% License
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
+%
+%
+% Author
+%  Gilles Degottex <degottex@csd.uoc.gr>
+%
 
 function x = interp1_extrapbounds(x)
 
