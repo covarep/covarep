52b3f8776f83a37f78b265e88472e4bc8b36c040
Gilles Degottex <degottex@csd.uoc.gr>
2013-07-26 21:38:34 +0300

    Fix header

--- sinusoidal/private/sin2sig.m
+++ sinusoidal/private/sin2sig.m
@@ -1,6 +1,31 @@
-% Synthesis of a time signal given sinusoids
-
-% sins: matrix of size(3,N) [freq;amp;phase]
+% Synthesis of a time signal given sinusoids parameters
+%
+% Inputs
+%  sins   : matrix of size(3,N) [freq;amp;phase]
+%  fs     : [Hz] Sampling frequency
+%  winlen : length of the time signal to synthesize
+%  derive : if deriv=1, synthesize the derivative of the signal
+%
+% Outputs
+%  s       : The synthesized signal
+%
+% Copyright (c) 2012 University of Crete - Computer Science Department
+% 
+% License
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
+%
+% Author
+%  Gilles Degottex <degottex@csd.uoc.gr>
+%
 
 function [s t] = sin2sig(sins, fs, winlen, deriv)
     if nargin<4; deriv=0; end
