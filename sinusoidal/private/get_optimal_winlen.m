8964863ec7b7714524930ebcbed3a5b7fe02544d
Gilles Degottex <degottex@csd.uoc.gr>
2013-07-26 21:33:46 +0300

    Fix header

--- sinusoidal/private/get_optimal_winlen.m
+++ sinusoidal/private/get_optimal_winlen.m
@@ -1,5 +1,24 @@
-% Get the winlen
-% ... while adjusting it to the min f0 in the window
+% Get the winlen while adjusting it to the minimum f0 within the window
+% 
+% Copyright (c) 2012 University of Crete - Computer Science Department
+% 
+% License
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
+%
+%
+% Author
+%  Gilles Degottex <degottex@csd.uoc.gr>
+%
+
 function winlen = get_optimal_winlen(f0s, fs, ind, opt)
 
     winf0 = f0s(ind,2); % the f0 corresponding to the window length
