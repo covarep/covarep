b4732329b686f9232083bf4f869f0e521742b278
Gilles Degottex <degottex@csd.uoc.gr>
2013-07-26 21:47:28 +0300

    Fix header

--- sinusoidal/private/spec_getsins_f0.m
+++ sinusoidal/private/spec_getsins_f0.m
@@ -1,5 +1,6 @@
 % Estimate of partials frequency, amplitude and phase given a gross fundamental frequency
 %
+% Description
 %  Estimate of partials frequency, amplitude and phase
 %  All returned partials frequencies are relative to spectrum size !
 %
@@ -29,22 +30,22 @@
 %  f0           : refined fundamental frequency
 %
 % Copyright (c) 2011 University of Crete - Computer Science Department
-%
+% 
 % License
-%  This file is part of libphoni. libphoni is free software: you can
-%  redistribute it and/or modify it under the terms of the GNU Lesser General
-%  Public License as published by the Free Software Foundation, either version 3
-%  of the License, or (at your option) any later version. libphoni is
-%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-%  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-%  details. You should have received a copy of the GNU Lesser General Public
-%  License along with libphoni. If not, see <http://www.gnu.org/licenses/>.
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
 %
 % Author
 %  Gilles Degottex <degottex@csd.uoc.gr>
-%  
-% $Id: spec_getsins_f0.m 142 2012-07-31 15:53:02Z degottex $
+%
 
 function sins = spec_getsins_f0(S, fs, f0, max_h, varargin)
 
