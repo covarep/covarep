5851a52c59eb8ad1881338ba98fb51cc99a53357
traitio <tuomo.raitio@aalto.fi>
2013-09-05 11:47:42 +0300

    Added LGPL statement

--- glottalsource/iaif_gci.m
+++ glottalsource/iaif_gci.m
@@ -36,7 +36,14 @@
 % Copyright (c) 2013 Aalto University
 %
 % License
-%  Please contact the author or the Copyright owner for any IP related question.
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
 %
 % This function is part of the Covarep project: http://covarep.github.io/covarep
 %
