b8a5129b30e70da832fc378c5211d60cd0cc6b86
traitio <tuomo.raitio@aalto.fi>
2013-09-05 11:46:50 +0300

    Update iaif.m

--- glottalsource/iaif.m
+++ glottalsource/iaif.m
@@ -37,16 +37,14 @@
 % Copyright (c) 2013 Aalto University
 %
 % License
-%  This code is a part of the GLOAT toolbox with the following
-%  licence:
-%  This program is free software: you can redistribute it and/or modify
-%  it under the terms of the GNU General Public License as published by
-%  the Free Software Foundation, either version 3 of the License, or
-%  (at your option) any later version.
-%  This program is distributed in the hope that it will be useful,
-%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%  GNU General Public License for more details.
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
 %
 % This function is part of the Common Speech Processing Repository 
 % http://covarep.github.io/covarep/
