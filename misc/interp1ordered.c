dc657cdf8dab7e87f404e08e59184f13c5d477c7
Gilles Degottex <gilles.degottex@gmail.com>
2014-10-25 20:48:58 +0200

    Add C implementation of linear interpolation

---
+++ misc/interp1ordered.c
@@ -0,0 +1,78 @@
+/* Fast linear interpolation of ordered values (see the Matlab version for doc)
+%
+% Copyright (c) 2012 University of Crete - Computer Science Department
+%
+% License
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
+%
+% Author
+%  Gilles Degottex <degottex@csd.uoc.gr>
+*/
+
+
+#include "mex.h"
+
+static void interp1ordered(double *yi, double *x, double *y, double *xi, double *yid, int m, int mi)
+{
+/*    printf("%i:\n",__LINE__); */
+    int i; /* The index of xi and yi */
+    int j; /* The index of x and y */
+    double g;
+
+    i=0;
+    j=0;
+    while(xi[i]<x[0]){
+        yi[i] = *yid;
+        i++;
+    }
+
+    for(; i<mi; i++)
+	{
+        while(j<m-1 && x[j+1]<xi[i])
+            j++;
+
+/* printf("j=%i %i\n",j,m); */
+        if(j==m-1)
+            break;
+
+        g = (xi[i]-x[j])/(x[j+1]-x[j]);
+/* printf("i=%i\n",i); */
+        yi[i] = y[j]*(1-g) + y[j+1]*g;
+	}
+
+	while(i<mi){
+        yi[i] = *yid;
+        i++;
+    }
+} 
+
+ /* Entry point */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    int m, mi;
+    double *yi;
+    double *x, *y, *xi, *yid;
+
+   m = mxGetNumberOfElements(prhs[0]);  /* Number of input data */
+   mi = mxGetNumberOfElements(prhs[2]); /* Number of data to interpolate */
+   plhs[0] = mxCreateDoubleMatrix(1,mi,mxREAL); /* output vector */
+
+   x       = mxGetPr(prhs[0]);  /* Reference abscissa */
+   y       = mxGetPr(prhs[1]);  /* Reference ordinates */
+   xi      = mxGetPr(prhs[2]);  /* Abscissa where to interpolate */
+   yid = mxGetPr(prhs[3]);      /* Default value */
+   yi = mxGetPr(plhs[0]);       /* Interpolated output values */
+
+   interp1ordered(yi, x, y, xi, yid, m, mi);
+
+   return;
+}
---
+++ misc/interp1ordered.m
@@ -0,0 +1,69 @@
+% Fast linear interpolation of ordered values (one dimendionnal)
+%
+% This matlab version is made to illustrate the behavior of the C version.
+% Do not expect speed from this Matlab version  !
+% Also, if this function is updated, its C version has to be updated
+% accordingly.
+%
+% Note tha the C version makes absolutely NO check at all.
+% Thus, it can crash very easily. 
+%
+% Build the C version using
+%  $ mex interp1ordered.c
+%
+% Input
+%  x   ; [Nx1] Ordered reference abscissa
+%  y   ; [Nx1] Ordered reference ordinates
+%  xi  : [Mx1] Ordered abscissa where to interpolate
+%  yid : [Mx1] Default out of bounds value
+%
+% Output
+%  yi  : Interpolated output values
+%
+% Copyright (c) 2012 University of Crete - Computer Science Department
+%
+% License
+%  This file is under the LGPL license,  you can
+%  redistribute it and/or modify it under the terms of the GNU Lesser General 
+%  Public License as published by the Free Software Foundation, either version 3 
+%  of the License, or (at your option) any later version. This file is
+%  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+%  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+%  PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+%  details.
+%
+% This function is part of the Covarep project: http://covarep.github.io/covarep
+%
+% Author
+%  Gilles Degottex <degottex@csd.uoc.gr>
+
+function yi = interp1ordered(x, y, xi, yid)
+
+    yi = yid*ones(size(xi));
+
+    i=1;
+    j=1;
+
+    while xi(i)<x(1)
+        yi(i) = yid;
+        i=i+1;
+    end
+
+    while i<=length(xi)
+        while j<length(x) && x(j+1)<xi(i);
+            j=j+1;
+        end
+        if j==length(x); break; end
+
+        g = (xi(i)-x(j))./(x(j+1)-x(j));
+
+        yi(i) = y(j,:)*(1-g) + y(j+1,:)*g;
+
+        i=i+1;
+    end
+
+    if i<=length(xi)
+        yi(i:end) = yid;
+    end
+
+return
