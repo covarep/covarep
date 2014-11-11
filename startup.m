829fd0f4dac34c07089984c07984776853d9d0af
Gilles Degottex <gilles.degottex@gmail.com>
2014-11-11 14:28:53 +0000

    use more generic path cleaner

--- startup.m
+++ startup.m
@@ -2,13 +2,17 @@
 
 addpath(genpath(pwd));
 
-rmpath(genpath([pwd '/.git']));
+% Remove all .git entries from the path ...
+entries = regexp(path, ['[^',pathsep,']+'],'match');
+idx = find(~cellfun(@isempty,regexp(entries,'\.git')) | ~cellfun(@isempty,regexp(entries,'\.svn')));
+if ~isempty(idx); rmpath(entries{idx}); end
 
+% ... s well as the documentation folder
 rmpath(genpath([pwd '/documentation']));
 
 if ~exist('OCTAVE_VERSION')
     % Matlab is running
-    % Thus remove the ocatve helper functions
+    % Thus remove also the octave helper functions
     rmpath(genpath([pwd '/external/octave_only']));
 else
     % Octave is running
